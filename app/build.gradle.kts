4a89d4cf90628c4248be34caa03a2538a2e39186
XiaoMouz <1114096791@qq.com>
2023-11-20 00:55:23 +0800

    ci: add new depency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,7 +52,6 @@ android {
 dependencies {
 
     implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.activity:activity-compose:1.8.0")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
     implementation("androidx.compose.ui:ui")
@@ -67,6 +66,13 @@ dependencies {
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
 
+    // Lifecycle
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+    implementation ("android.arch.lifecycle:extensions:1.1.1")
+
+    // Lifecycle of Viewmodel
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
+
     // Material
     implementation("androidx.compose.material:material:1.5.3")
 
@@ -79,9 +85,6 @@ dependencies {
 
     // Navigation
     implementation("androidx.navigation:navigation-compose:2.7.4")
-
-    // Viewmodel
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
 
     // Retorfit: Network client
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
